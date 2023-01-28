ccc994fb46667c5d60351558fd11c692bec3a6d8
YarBurArt <yaroslav.chayk@gmail.com>
2023-01-28 13:02:13 +0500

    clean code

--- rei/reiapi/serializers.py
+++ rei/reiapi/serializers.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 
 from .models import Question
 
+# reform data from database to byte for tcp/udp protocol
+
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
-        fields = ('request_char', 'answer_char', 'response_date')
+        fields = ('id', 'request_char', 'answer_char', 'request_date')
